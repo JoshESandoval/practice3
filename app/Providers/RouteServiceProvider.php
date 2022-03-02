e0887513a63b543a502c4d078594f2fd3366c5db
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-02 14:18:09 -0800

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,10 +26,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if($this->app->environment('production')){
-            /URL::forceScheme('https');
-        }
-        
+       
         $this->configureRateLimiting();
 
         $this->routes(function () {
