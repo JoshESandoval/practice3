217a2babadb1f9cfc10e5b2bea8e0debeab8ff4f
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-03 20:46:52 -0800

    Delete Cors.php

--- app/Http/Middleware/Cors.php
+++
@@ -1,5 +0,0 @@
-public function handle($request, Closure $next){
-  return $next($request)
-    ->header('Access-Control-Allow-Orgin', '*')
-    ->header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-}
