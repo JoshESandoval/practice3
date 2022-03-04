228e37b1257f192c4977bb081ddd04029f228f85
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-03 20:00:56 -0800

    Create errorPage.blade.php

---
+++ resources/views/errorPage.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'oh no, our table')
+
+@section('content_header')
+  <h1>Something Went wrong</h1>
+@stop
+
+@section('content')
+  <div>Please watch my apology video at ... </div>
+  <div>Like, subscribe, I'm sorry, and don't forget to like</div>
+@stop
+
+@section('css')
+  <link rel="stylesheet" href="css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>console.log('sup');</script>
+@stop
