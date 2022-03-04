4e2111ffc3253cad4eecf558f2af2751fb3b085a
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-03 21:30:02 -0800

    Update todos.blade.php

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -7,12 +7,31 @@
 @stop
 
 @section('content')
-  <div>
-    <ul>
-      <li>Learn postgres</li>
-      <li>Submit Homework</li>
-      <li>Make website</li>
+<div class="card">
+  <div class="card-body">
+    <table id="table" class="table table-bordered">
+      <thead>
+        <tr>
+          <th style="width: 10px">#</th><th>Task</th><th>Progress</th><th style="width: 40px">Label</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1.</td>
+          <td>Update software</td>
+          <td><div class="progress progress-xs"><div class="progress-bar progress-bar-danger" style="width: 55%"></div></div></td>
+          <td><span class="badge bg-danger">55%</span></td>
+        </tr>
+        <tr>
+          <td>2.</td>
+          <td>Clean database</td>
+          <td><div class="progress progress-xs"><div class="progress-bar bg-warning" style="width: 70%"></div></div></td>
+          <td><span class="badge bg-warning">70%</span></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
+</div>
 @stop
 
 @section('css')
@@ -20,5 +39,7 @@
 @stop
 
 @section('js')
-    <script>console.log('sup');</script>
+    <script>$(document).ready(function() {
+        $('#table').DataTable();
+    } );</script>
 @stop
