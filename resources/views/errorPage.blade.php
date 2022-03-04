4b9434eadd8177b39b423ab01877c420893bde8b
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-03 23:34:19 -0800

    Update errorPage.blade.php

--- resources/views/errorPage.blade.php
+++ resources/views/errorPage.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'oh no, our table')
 
 @section('content_header')
-  <h1 style="color:yellow, outline: black 2px">Something Went wrong Error 404</h1>
+  <h1 style="color:yellow; font-weight:bold;">Something Went wrong Error 404</h1>
 @stop
 
 @section('content')
