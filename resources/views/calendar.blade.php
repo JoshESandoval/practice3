a86016960d1581e17e3c9087792244918fd2dc43
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-03 22:50:58 -0800

    Update calendar.blade.php

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -7,16 +7,8 @@
 @stop
 
 @section('content')
-<div class="row">
-  <div class="col-md-12">
-    <div class="card card-primary">
-      <div class="card-body p-0">
-        <div id="calendar" class="fc fc-media-screen fc-direction-ltr fc-theme-bootstrap">
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+
+
 @stop
 
 @section('css')
@@ -24,15 +16,15 @@
 @stop
 
 @section('js')
-<script src="//cdn.jsdelivr.net/npm/fullcalendar@5.10.2/main.min.js" ></script>
-<script>
-$( document ).ready(function() {
-    var calendar = new FullCalendar.Calendar($('#calendar')[0], {
-      initialView: 'dayGridMonth',
-      events: '/events-feed'
-    });
+  <script src="//cdn.jsdelivr.net/npm/fullcalendar@5.10.2/main.min.js" ></script>
+  <script>
+  $( document ).ready(function() {
+      var calendar = new FullCalendar.Calendar($('#calendar')[0], {
+       initialView: 'dayGridMonth',
+       events: '/events-feed'
+       });
     calendar.render();
-})
+  })
 </script>
 
 @stop
