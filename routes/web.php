07c7638f08a9a93eedfba9f169d043dd6919f909
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-03 21:16:43 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::middleware(['cors'])->group(function () {
-    Route::post('/heroku', 'Controller@heroku');
+    Route::post('/herokuapp', 'Controller@herokuapp');
 });
 
 Route::get('/', function () {
