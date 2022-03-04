0af47649941038455f87c84d286bef5e97f8705f
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-03 19:43:46 -0800

    Create board.blade.php

---
+++ resources/views/board.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@sectin('title, 'Board')
+
+@section('content_header')
+  <h1>Main Board</h1>
+@stop
+
+@section('content')
+  <p>I could make a joke but you already know where it goes</>
+@stop
+
+@section('css')
+  <link rel="stylesheet" href="css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>console.log('sup');</script>
+@stop
