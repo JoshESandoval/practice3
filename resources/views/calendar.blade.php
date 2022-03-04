9a306b385af7e411a882cbb24f9341e8d2fe394f
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-03 19:42:33 -0800

    Create calendar.blade.php

---
+++ resources/views/calendar.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@sectin('title, 'Calendar')
+
+@section('content_header')
+  <h1>Calendar</h1>
+@stop
+
+@section('content')
+  <div>Play video games and code alot</div>
+@stop
+
+@section('css')
+  <link rel="stylesheet" href="css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>console.log('yo yo');</script>
+@stop
