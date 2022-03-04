6073b9531850417d2904f4a2a040b23683de53ba
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-03 23:01:38 -0800

    Update calendar.blade.php

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -6,17 +6,8 @@
   <h1>Calendar</h1>
 @stop
 
-@section('content')
-<div class="row">
-  <div class="col-md-12">
-    <div class="card card-primary">
-      <div class="card-body p-0">
-        <div id="calendar" class="fc fc-media-screen fc-direction-ltr fc-theme-bootstrap">
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+@section('FullCalendar', true)
+
 
 @stop
 
