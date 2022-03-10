7fde35d3fb5f127622af08b33f74db91158fa05d
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-09 17:29:14 -0800

    Update 2022_03_10_011156_create_todos_table.php

--- database/migrations/2022_03_10_011156_create_todos_table.php
+++ database/migrations/2022_03_10_011156_create_todos_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->tinyInterger('progress');
+            $table->tinyInteger('progress');
             $table->timestamps();
         });
     }
