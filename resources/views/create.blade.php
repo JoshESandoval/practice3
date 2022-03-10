20cb0d85d70fd165c40917abd8c4832809486d72
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-10 01:33:59 -0800

    mann

---
+++ resources/views/create.blade.php
@@ -0,0 +1,16 @@
+@extends('adminlte::page')
+
+@section('title', 'To do List')
+
+@section)'content_header')
+	<h1>To Do's </h1>
+@stop
+
+@section('content')
+<form method="post" action="{{route('todos.store") }} >
+	@csrf
+	<x-adminlte-input name="title" label="Title" />
+	<x-adminlte-input name="progress" type = "number" min=- max=100 label="Progress" />
+	<x-adminlte-button type="Submit" label="Submit" />
+</form>
+@stop	
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
 header("Access-Control-Allow-Origin: *");
 
@@ -48,7 +49,7 @@ Route::get('/board', function () {
     return view('board');
 });
 
-
+Route::resource('/todos', TodoController::class);
 
 
 Route::fallback( function () {
