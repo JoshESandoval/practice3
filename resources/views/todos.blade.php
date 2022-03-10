81564e405c7af41e66ab5aa15a8d15c060545dcb
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-09 18:26:00 -0800

    Update todos.blade.php

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -18,28 +18,24 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1.</td>
-          <td>Learn postgres</td>
-          <td><div class="progress progress-xs"><div class="progress-bar progress-bar-danger" style="width: 5%"></div></div></td>
-          <td><span class="badge bg-danger">5%</span></td>
-        </tr>
-        <tr>
-          <td>2.</td>
-          <td>Clean room</td>
-          <td><div class="progress progress-xs"><div class="progress-bar bg-warning" style="width: 90%"></div></div></td>
-          <td><span class="badge bg-warning">90%</span></td>
-        </tr>
-        <tr>
-          <td>3.</td>
-          <td>Finish Homework</td>
-          <td><div class="progress progress-xs"><div class="progress-bar bg-normal" style="width: 70%"></div></div></td>
-          <td><span class="badge bg-good">70%</span></td>
-        </tr>
+        @foreach($todos AS $todo)
+            <td>{{$todo->id }}</td>
+            <td>{{$todo->title}}</td>
+            <td>
+                <div class = "progress progress-xs">
+                    <div class= "progress-bar progress-bar-danger" style="width : {{$todo->progress}}%">
+                    </div>
+                </div>
+            </td>
+            <td>
+                <a class = "btn btn-default btn-sm" href="{{route('todos.show',['todo'=>$todo->id])}}">View</a>
+            </td>
+            @endforeach
       </tbody>
     </table>
   </div>
 </div>
+<a href="{{route('todos.create')}} " class="btn btn-primary">Create</a>
 @stop
 
 @section('css')
