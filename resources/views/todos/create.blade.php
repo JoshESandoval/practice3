99035d30fc8766ce08055313311e721f23cd3495
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-10 02:23:03 -0800

    Update create.blade.php

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -10,7 +10,7 @@
 <form method="post" action="{{route('todos.store') }}" >
 	@csrf
 	<x-adminlte-input name="title" label="Title" />
-	<x-adminlte-input name="progress" type = "number" min=- max=100 label="Progress" />
+	<x-adminlte-input name="progress" type = "number" min=0 max=100 label="Progress" />
 	<x-adminlte-button type="Submit" label="Submit" />
 </form>
 @stop	
