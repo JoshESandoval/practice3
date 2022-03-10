22db4cfe86277919b88a1b4f8788ec91018ff6dc
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-10 01:58:58 -0800

    data

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Eloquent\Factories\HasFactory;
-use Illuminate\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
