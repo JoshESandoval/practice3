b67822fee8d3524a38bba7347d28c761ebdc2055
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-11 03:52:36 -0800

    Update edit.blade.php

--- resources/views/todos/edit.blade.php
+++ resources/views/todos/edit.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 <form method="post" action={{route('todos.update')}}>
   <x-adminlte-input name='title' label='Title' placeholder={{$todo->title;}} />
-  <x-adminlte-input name='progress' label='progress' type='number min=0 max=100 placeholder={{$todo->progress;}} />
+  <x-adminlte-input name='progress' label='progress' type='number' min=0 max=100 placeholder={{$todo->progress;}} />
   <x-adminlte-button type="Submit" label="Submit" />
   </form>
 @stop
