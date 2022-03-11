796825ed9ccae8237bb3db6b59ae776c3311f83d
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-11 04:46:04 -0800

    Update edit.blade.php

--- resources/views/todos/edit.blade.php
+++ resources/views/todos/edit.blade.php
@@ -7,7 +7,7 @@
 @stop
 
 @section('content')
-<form method="post" action={{route('todos.update',$id)}}>
+<form method="post" action={{route('todos.update',$todo->id)}}>
    @csrf
   <x-adminlte-input name='title' label='Title' placeholder={{$todo->title;}} />
   <x-adminlte-input name='progress' label='progress' type='number' min=0 max=100 placeholder={{$todo->progress;}} />
