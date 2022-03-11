76cfc56d1e098281fbe1408665d618795087e349
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-11 05:45:51 -0800

    Update show.blade.php

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -9,5 +9,5 @@
 @section('content')
 	<h2>{{ $todo->title;}}</h2>
 	<div><p>{{$todo->progress;}}% finished</p></div>
-    <a  class = "btn btn-primary" href="{{ route('todos.edit', ['todo'=>$todo->id])}}" >Edit</a>
+    <a  class = "btn btn-default btn-sm" href="{{ route('todos.edit', ['todo'=>$todo->id] )}}" >Edit</a>
 @stop
