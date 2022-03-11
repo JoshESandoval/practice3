1a8d280a864bcbda8acbddef934d9d437833e279
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-11 03:58:08 -0800

    Update show.blade.php

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -9,5 +9,5 @@
 @section('content')
 	<h2>{{ $todo->title;}}</h2>
 	<div><p>{{$todo->progress;}}% finished</p></div>
-    <a href="{{ route('todos.edit' }}"  class = "btn btn-primary">Edit</a>
+    <a href="{{ route('todos.edit') }}"  class = "btn btn-primary">Edit</a>
 @stop
