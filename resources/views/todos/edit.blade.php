6f84a3f54c9bc5ffe6ae0ff92021b436cdbd8fb7
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-11 06:58:06 -0800

    Update edit.blade.php

--- resources/views/todos/edit.blade.php
+++ resources/views/todos/edit.blade.php
@@ -7,9 +7,8 @@
 @stop
 
 @section('content')
-<form method="post" action={{route('todos.update', $todo->id)}}>
+<form method="put" action={{route('todos.update', $todo->id)}}>
    @csrf
-   @method('PUT')
   <x-adminlte-input name="title" label="Title" value="{{$todo->title}}"/>
   <x-adminlte-input name='progress' label='progress' type='number' min=0 max=100 value="{{$todo->progress}}" />
   <x-adminlte-button type="Submit" label="Submit" />
