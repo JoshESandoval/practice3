6a11651451ab738f18c9f7f47e0bc7da132ec319
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-11 03:51:03 -0800

    Create edit.blade.php

---
+++ resources/views/todos/edit.blade.php
@@ -0,0 +1,15 @@
+@extends('adminelte::page')
+
+@section('title', 'To Do List')
+
+@section('content_header')
+  <h1>Edit To Do Item</h1>
+@stop
+
+@section('content')
+<form method="post" action={{route('todos.update')}}>
+  <x-adminlte-input name='title' label='Title' placeholder={{$todo->title;}} />
+  <x-adminlte-input name='progress' label='progress' type='number min=0 max=100 placeholder={{$todo->progress;}} />
+  <x-adminlte-button type="Submit" label="Submit" />
+  </form>
+@stop
