b5ab3c62b744b0df37f40ce5e2586c9bd957b03a
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-11 21:46:46 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,7 @@ Route::get('/board', function () {
 
 Route::get('/event-feed', function(){
     
-    $events = CalEventController::all('title', 'start_at AS start', 'end_at AS end')->get();
+    $events = CalEventController::class::all('title', 'start_at AS start', 'end_at AS end')->get();
 });
 
 Route::resource('/todos', TodoController::class);
