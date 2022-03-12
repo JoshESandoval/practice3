2b37c0b4b8b63cf4f78bfce601b68f42f3a06f20
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-11 20:22:08 -0800

    Update create.blade.php

--- resources/views/calendar/create.blade.php
+++ resources/views/calendar/create.blade.php
@@ -7,8 +7,9 @@
 @stop
 
 @section('content')
-    @csrf
+    
     <form method="post" action= "{{route('calendar.store')}}">
+        @csrf
         <x-adminlte-input name="title" label="Title"/>
         <h5>Start Time </br></h5>
         <input type="datetime-local" name="start_at"/>
