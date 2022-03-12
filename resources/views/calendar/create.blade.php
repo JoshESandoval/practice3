dd198d7573a390279918e6edf608fe4f4b935624
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-11 21:33:57 -0800

    Update create.blade.php

--- resources/views/calendar/create.blade.php
+++ resources/views/calendar/create.blade.php
@@ -13,10 +13,10 @@
         <x-adminlte-input name="title" label="Title"/>
         
         <h5>Start Time </br></h5>
-        <input type="datetime-local" name="start_at"/>
+        <input type="date" name="start_at"/>
         
         <h5>End Time </br></h5>
-        <input type="datetime-local" name="end_at"/>
+        <input type="date" name="end_at"/>
         
         <x-adminlte-button type="Submit" label="submit"/>
     </form>
