db57ee31539720e89c2d6c418521acf17b775294
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-11 23:17:16 -0800

    Update TodoController.php

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -104,6 +104,6 @@ class TodoController extends Controller
     {
         $todo = Todo::find($id);
         $todo->delete();
-        return view('todos');
+        redirect()->route('todos');
     }
 }
