258f1c344b988b57a3f15faa48539b35ff4589dc
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-11 18:11:06 -0800

    Update create.blade.php

--- resources/views/calendar/create.blade.php
+++ resources/views/calendar/create.blade.php
@@ -9,9 +9,9 @@
 @section('content')
     <form method="post" action= "{{route('calendar.store')}}">
         <x-adminlte-input name="title" label="Title"/>
-        <h4>Start Time </br></h4>
+        <h5>Start Time </br></h5>
         <input type="datetime-local" name="start_time"/>
-        <h4>End Time </br></h4>
+        <h5>End Time </br></h5>
         <input type="datetime-local" name="end_time"/>
     </form>
 @stop	
