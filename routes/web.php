238259b0b889426039b637f8afbb463bd159161f
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-11 16:38:48 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,7 @@ Route::get('/board', function () {
 
 Route::resource('/todos', TodoController::class);
 
-Route::resource('/calendar', CalEventController::class);
+
 
 Route::fallback( function () {
     return view('errorPage');   
