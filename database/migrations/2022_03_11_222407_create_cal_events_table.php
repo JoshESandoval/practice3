b82ded534b071d3891d6c57bc8024870ad64d272
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-11 16:50:15 -0800

    Update 2022_03_11_222407_create_cal_events_table.php

--- database/migrations/2022_03_11_222407_create_cal_events_table.php
+++ database/migrations/2022_03_11_222407_create_cal_events_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('cal_events', function (Blueprint $table) {
             $table->id();
             $title->string('title');
-            $start->dateTime('start_at', setprecision=0 );                        
-            $end->dateTime('end_at', setprecision=0 );
+            $start->dateTime('start_at', $precision=0 );                        
+            $end->dateTime('end_at', $precision = 0 );
             $table->timestamps();
         });
     }
