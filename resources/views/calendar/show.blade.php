a4feb40f1a3bbc92e4215ef03b07ee61068ebe07
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-11 23:53:21 -0800

    Update show.blade.php

--- resources/views/calendar/show.blade.php
+++ resources/views/calendar/show.blade.php
@@ -11,6 +11,5 @@
 	<h2>{{ $cal_event->title;}}</h2>
     <p>Start at {{ $cal_event->start_at;}}</p>
     <p>End at {{ $cal_event->end_at;}}</p>
-    <a  class = "btn btn-default btn-sm" href="{{ route('calendar.edit', ['cal_event'=>$cal_event->id] )}}" >Edit</a>
-    
+       
 @stop
